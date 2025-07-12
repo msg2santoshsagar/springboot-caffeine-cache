40abd2dffe4bbdeb462d925a0f187a904c277512
Santosh Sagar <msg2santoshsagar@gmail.com>
2025-07-12 09:25:19 +0530

    Application bootstraped

---
+++ src/test/java/com/alfie/springbootcaffeinecache/SpringbootCaffeineCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alfie.springbootcaffeinecache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCaffeineCacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
